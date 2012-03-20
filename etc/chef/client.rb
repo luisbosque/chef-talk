6a22b744697df7f92efdb5734e0e20e131bbe015
Luis Bosque <luisico@gmail.com>
2012-03-21 00:04:46 +0100

    client.rb

---
+++ etc/chef/client.rb
@@ -0,0 +1,4 @@
+log_level        :info
+log_location     '/var/log/chef/client.log'
+chef_server_url  "https://api.opscode.com/organizations/ORGANIZATION"
+validation_client_name "ORGANIZATION-validator"
